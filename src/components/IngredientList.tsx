2fcb9c698f9191375a15dd264288c107d32eeaf3
Nathan Hildebrandt <541nathan@gmail.com>
2025-02-03 16:53:29 -0800

    Fix wrong grams number for chicken

--- src/components/IngredientList.tsx
+++ src/components/IngredientList.tsx
@@ -8,7 +8,7 @@ interface IngredientListProps {
 
 const IngredientList: React.FC<IngredientListProps> = ({ days }) => {
   const ingredients = {
-    chicken: days * 296, // 296 grams of chicken per day
+    chicken: days * 269, // 296 grams of chicken per day
     canola: days * 16,
     rice: days * 119,
     apples: days * 61, 
@@ -25,20 +25,20 @@ const IngredientList: React.FC<IngredientListProps> = ({ days }) => {
         <thead>
           <tr>
             <th>Ingredient</th>
-            <th>Amount<br/>(grams)</th>
-            <th>Amount<br/>(tsp)</th>
+            <th>Grams</th>
+            <th>Volume</th>
           </tr>
         </thead>
         <tbody>
-        <IngredientItem name="Chicken" amount={ingredients.chicken} />
+        <IngredientItem name="Chicken" amount={ingredients.chicken} volume={(ingredients.chicken/453.592).toFixed(2)+" lbs"} />
         <IngredientItem name="Canola Oil" amount={ingredients.canola} />
         <IngredientItem name="Rice" amount={ingredients.rice} />
         <IngredientItem name="Apples" amount={ingredients.apples} />
         <IngredientItem name="Carrots" amount={ingredients.carrots} />
         <IngredientItem name="Kale" amount={ingredients.kale} />
-        <IngredientItem name="Salt" amount={(ingredients.salt).toFixed(2)} volume={(days * 0.4).toFixed(2)} />
-        <IngredientItem name="Omega-3" amount={(ingredients.omega3).toFixed(2)} volume={(days * 0.191356628461).toFixed(2)} />
-        <IngredientItem name="BalanceIT" amount={(ingredients.balanceit).toFixed(2)} volume={(days * 3.363894).toFixed(2)}/>
+        <IngredientItem name="Salt" amount={(ingredients.salt).toFixed(2)} volume={(days * 0.4).toFixed(2)+" tsp"} />
+        <IngredientItem name="Omega-3" amount={(ingredients.omega3).toFixed(2)} volume={(days * 0.191356628461).toFixed(2)+" tsp"} />
+        <IngredientItem name="BalanceIT" amount={(ingredients.balanceit).toFixed(2)} volume={(days * 3.363894).toFixed(2)+" tsp"}/>
         </tbody>
       </table>
     </div>
