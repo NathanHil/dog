8adac233259eb64250b9ba62930435007532c78f
Nathan Hildebrandt <541nathan@gmail.com>
2026-02-28 17:37:02 -0800

    Clean up totals and numbers

--- src/components/IngredientList.tsx
+++ src/components/IngredientList.tsx
@@ -8,7 +8,7 @@ interface IngredientListProps {
 
 const IngredientList: React.FC<IngredientListProps> = ({ days }) => {
   const ingredients = {
-    chicken: days * 259.65, // 301 grams of chicken per day
+    chicken: days * 259.45, // 301 grams of chicken per day
     canola: days * 15.52,
     rice: days * 110.4,
     uncookedRice: days * 0.1948,
@@ -17,9 +17,12 @@ const IngredientList: React.FC<IngredientListProps> = ({ days }) => {
     kale: days * 19.84,
     salt: days * 2.59,
     omega3: days * 0.794,
-    balanceit: days * 8.59,
-    total: days * (259.65+15.52+110.4+0.1948+58.66+24.15+19.84+2.59+0.794+8.59)
+    balanceit: days * 8.59
   };
+  const totals = {
+    totalGrams: ingredients.chicken+ingredients.canola+ingredients.rice+ingredients.apples+ingredients.carrots+ingredients.kale+ingredients.salt+ingredients.omega3+ingredients.balanceit,
+    totalPerDay: (ingredients.chicken+ingredients.canola+ingredients.rice+ingredients.apples+ingredients.carrots+ingredients.kale+ingredients.salt+ingredients.omega3+ingredients.balanceit)/days
+  }
 
   return (
     <div className="ingredient-list">
@@ -42,7 +45,7 @@ const IngredientList: React.FC<IngredientListProps> = ({ days }) => {
         <IngredientItem name="Salt" amount={(ingredients.salt).toFixed(2)} volume={(days * 0.4).toFixed(2)+" tsp"} />
         <IngredientItem name="Omega-3" amount={(ingredients.omega3).toFixed(2)} volume={(days * 0.191356628461).toFixed(2)+" tsp"} />
         <IngredientItem name="BalanceIT" amount={(ingredients.balanceit).toFixed(2)} volume={(days * 0.07008).toFixed(1)+" Cups"}/>
-        <tr><td><strong>Total</strong></td><td>{ingredients.total.toFixed(2)} g</td><td><strong>{((ingredients.total / days)/2).toFixed(2)}g</strong> per meal</td></tr>
+        <tr><td><strong>Total</strong></td><td>{totals.totalGrams.toFixed(0)} g</td><td><strong>{((totals.totalPerDay)/2).toFixed(0)}g</strong> per meal ({days*2})</td></tr>
         </tbody>
       </table>
     </div>
